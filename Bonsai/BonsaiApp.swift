717b1a007fbf54251b9bc590185a550be135a6c8
aaronkjin <58490258+hoxykidmilli@users.noreply.github.com>
2023-08-24 01:09:12 -0700

    Initial Commit

---
+++ Bonsai/BonsaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  BonsaiApp.swift
+//  Bonsai
+//
+//  Created by Aaron Jin on 8/24/23.
+//
+
+import SwiftUI
+
+@main
+struct BonsaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BonsaiUITests/BonsaiUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BonsaiUITestsLaunchTests.swift
+//  BonsaiUITests
+//
+//  Created by Aaron Jin on 8/24/23.
+//
+
+import XCTest
+
+final class BonsaiUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
